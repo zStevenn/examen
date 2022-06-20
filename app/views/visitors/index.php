f797c6724ca0deac04eb0c493fd8472b2c00f7f1
zStevenn <327068@student.mboutrecht.nl>
2022-06-20 17:18:04 +0200

    Created sign up form on views/visitors/signup.php

--- app/views/visitors/index.php
+++ app/views/visitors/index.php
@@ -1,1 +1,24 @@
 <h3><?= $data["title"]; ?></h3>
+<!-- Form for signing up for a lessonpackage -->
+<form action="#" method="POST">
+  <label>First Name</label>
+  <input type="text" placeholder="Enter first name" name="firstname" required>
+  <br>
+  <label>Infix</label>
+  <input type="text" placeholder="Enter infix" name="infix" required>
+  <br>
+  <label>Last name</label>
+  <input type="text" placeholder="Enter last name" name="lastname" required>
+  <br>
+  <label>Email address</label>
+  <input type="email" placeholder="Enter email" name="email" required>
+  <br>
+  <label>Lesson package</label>
+  <select name="lessonpackage">
+    <option value="x">x</option>
+  </select>
+  <br>
+  <button type="submit" value="submit">Submit</button>
+</form>
+
+<?php var_dump($_POST); ?>
