d57a0da08d252582dff5d5dafa66d10f2e7ea520
AronVLD <327156@student.mboutrecht.nl>
2022-06-21 09:55:11 +0200

    added files with old code for read page

---
+++ app/views/defects/index.php
@@ -0,0 +1,4 @@
+<?=require APPROOT . '/views/includes/header.php';?> 
+<p><h3><?= $data["title"]; ?></h3></p>
+<a href="<?=URLROOT;?>/activestudents/index" class="btn btn-success">ActiveStudents</a>
+
