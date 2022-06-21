4f0202791534bbbdd32c37b9f35954dabdd44a89
QEUN <332275@student.mboutrecht.nl>
2022-06-21 15:48:19 +0200

    fixed an isue with the insert

--- app/views/callsicknes/index.php
+++ app/views/callsicknes/index.php
@@ -2,12 +2,17 @@
 <form method="POST"
       action="<?php echo URLROOT; ?>/CallSicknes/index">
     <?php echo $data['emailError']; ?>
-
     <br>
     <label>E-mail</label>
-    <input type="email" name="email" placeholder="Type E-mail">
+    <select name="email">
+        <option selected disabled> select</option>
+        <option value="a@a.nl">a</option>
+        <option value="f@f.nl">f </option>
+        <option value="c@c.nl">c </option>
+        <option value="s@s.nl">s </option>
+        <option value="v@v.nl">v </option>
+    </select>
     <br><br>
-
 
     <label>Reason</label>
     <input type="text" name="reason" placeholder="Type reason">
